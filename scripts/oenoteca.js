13cb7babbb480579e24eba60f42a788205b177c8
Chris Kark <ckark@alumni.stanford.edu>
2017-11-09 14:18:11 -0800

    Update oenoteca.js

--- scripts/oenoteca.js
+++ scripts/oenoteca.js
@@ -24,3 +24,23 @@ $(function() {
                 }
         });
 });
+$(function() {
+    "use strict";
+    var $navbar = $(".container-nav"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > y_pos + height) {
+            $navbar.addClass("navbar-fixed").animate({
+                top: 0
+            });
+        } else if (scrollTop <= y_pos) {
+            $navbar.removeClass("navbar-fixed").clearQueue().animate({
+                top: "-30px"
+            }, 0);
+        }
+    });
+});
