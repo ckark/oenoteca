a130579341a5af50d7275906864970df7d05e664
Chris Kark <ckark@alumni.stanford.edu>
2017-11-10 07:22:39 -0800

    Update oenoteca.js

--- scripts/oenoteca.js
+++ scripts/oenoteca.js
@@ -1,46 +1,47 @@
+/* Toggle Navbar Menu on Small Viewports */
 $(function() {
-        var navLinks = document.querySelector('.narrow');
-        navLinks.addEventListener('click', toggle);
-        var narrowlinks = document.querySelector('.narrowlinks');
+	var navLinks = document.querySelector('.narrow');
+	navLinks.addEventListener('click', toggle);
+	var narrowlinks = document.querySelector('.narrowlinks');
 
-        function toggle() {
-                narrowlinks.classList.toggle('hidden');
-        };
+	function toggle() {
+		narrowlinks.classList.toggle('hidden');
+	};
 });
+/* Sticky Navbar */
 $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
-                if (location.pathname.replace(/^\//, '') == this.pathname.replace(
-                                /^\//, '') || location.hostname == this.hostname) {
-                        var target = $(this.hash);
-                        target = target.length ? target : $('[name=' + this.hash.slice(
-                                1) + ']');
-                        if (target.length) {
-                                $('html,body').animate({
-                                        scrollTop: target.offset().top -
-                                                0
-                                }, 1000);
-                                return false;
-                        }
-                }
-        });
+	"use strict";
+	window.addEventListener("hashchange", function() {
+		scrollBy(0, -50)
+	})
+	var $navbar = $(".container-nav"),
+		y_pos = $navbar.offset().top,
+		height = $navbar.height();
+	$(document).scroll(function() {
+		var scrollTop = $(this).scrollTop();
+		if (scrollTop > y_pos + height) {
+			$navbar.addClass("navbar-sticky").animate({
+				top: "-1px"
+			});
+		} else if (scrollTop <= y_pos) {
+			$navbar.removeClass("navbar-sticky").clearQueue().animate({
+				top: "-30px"
+			}, 0);
+		}
+	});
 });
+/* Smooth Scrolling */
 $(function() {
-    "use strict";
-    var $navbar = $(".container-nav"),
-        y_pos = $navbar.offset().top,
-        height = $navbar.height();
-
-    $(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-
-        if (scrollTop > y_pos + height) {
-            $navbar.addClass("navbar-fixed").animate({
-                top: 0
-            });
-        } else if (scrollTop <= y_pos) {
-            $navbar.removeClass("navbar-fixed").clearQueue().animate({
-                top: "-30px"
-            }, 0);
-        }
-    });
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top - 96
+				}, 1000);
+				return false;
+			}
+		}
+	});
 });
